43e29c7451e886ae6e6766200826308c2e87c2d9
Arif <faizin.arief@gmail.com>
2020-03-27 14:07:26 +0700

    Add: Test Room via Dagger without UI

---
+++ app/src/androidTest/java/com/arif/jetpackpro/fragment/DatabaseTestModule.kt
@@ -0,0 +1,25 @@
+package com.arif.jetpackpro.fragment
+
+import android.content.Context
+import androidx.room.Room
+import com.arif.jetpackpro.room.MovieDao
+import com.arif.jetpackpro.room.MovieDatabase
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+//TODO 3 : Add @Module - Ngasih tau dagger bagaimana suatu kelas terbentuk
+@Module
+class DatabaseTestModule {
+    @Singleton // We only want one retrofit instance at any time
+    @Provides // Tells Dagger that the Room instance will come from here
+    fun provideDatabase(context: Context): MovieDatabase =  Room.inMemoryDatabaseBuilder(context, MovieDatabase::class.java)
+        .allowMainThreadQueries()
+        .build()
+
+    @Singleton
+    @Provides
+    //karena MovieDao adalah interface maka harus pakai Provides
+    fun provideMovieDao(database: MovieDatabase): MovieDao = database.movieDao()
+}
+
