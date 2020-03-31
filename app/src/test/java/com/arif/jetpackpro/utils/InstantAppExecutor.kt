a41951af7f11dd36fcef7075d5800325ce0a673f
Arif <faizin.arief@gmail.com>
2020-03-31 13:19:12 +0700

    Fix executor

--- app/src/test/java/com/arif/jetpackpro/utils/InstantAppExecutor.kt
+++ app/src/test/java/com/arif/jetpackpro/utils/InstantAppExecutor.kt
@@ -4,7 +4,7 @@ import com.arif.jetpackpro.util.AppExecutors
 import java.util.concurrent.Executor
 
 
-class InstantAppExecutors : AppExecutors(instant, instant) {
+class InstantAppExecutors : AppExecutors(instant, instant, instant) {
     companion object {
         private val instant = Executor { it.run() }
     }
