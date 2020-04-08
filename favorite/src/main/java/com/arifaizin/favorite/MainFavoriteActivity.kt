7b53b9d1ab0a576c562d71e716bfc22dde1b3797
Arif <faizin.arief@gmail.com>
2020-04-08 12:11:35 +0700

    Add: favorite - dynamic feature

---
+++ favorite/src/main/java/com/arifaizin/favorite/MainFavoriteActivity.kt
@@ -0,0 +1,46 @@
+package com.arifaizin.favorite
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.arifaizin.favorite.fragment.FavoriteFragment
+import kotlinx.android.synthetic.main.activity_main_favorite.*
+
+class MainFavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_favorite)
+
+        supportActionBar?.hide()
+        setupViewPager()
+    }
+
+    private fun setupViewPager() {
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        view_pager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(view_pager)
+    }
+
+    class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        private val tabTitles = arrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2
+        )
+
+        override fun getItem(position: Int): androidx.fragment.app.Fragment {
+            return FavoriteFragment.newInstance(position + 1)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return context.resources.getString(tabTitles[position])
+        }
+
+        override fun getCount(): Int {
+            return 2
+        }
+    }
+}
