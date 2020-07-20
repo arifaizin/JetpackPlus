3e6668ef1329e747732b45deb7c3841cf85c8b6b
Arif <faizin.arief@gmail.com>
2020-07-20 17:11:04 +0700

    Add: certificate pinning

--- app/src/main/java/com/arif/jetpackpro/retrofit/ApiConfig.kt
+++ app/src/main/java/com/arif/jetpackpro/retrofit/ApiConfig.kt
@@ -2,15 +2,23 @@ package com.arif.jetpackpro.retrofit
 
 import com.arif.jetpackpro.BuildConfig
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 object ApiConfig {
 
+    var hostname = "themoviedb.org"
+    private var certificatePinner = CertificatePinner.Builder()
+        .add(hostname, "sha256/HkCBucsA3Tgiby96X7vjb/ojHaE1BrjvZ2+LRdJJd0E=")
+        .add(hostname, "sha256/nKWcsYrc+y5I8vLf1VGByjbt+Hnasjl+9h8lNKJytoE=")
+        .build()
     private var client = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .certificatePinner(certificatePinner)
         .build()
 
     private fun getRetrofit(): Retrofit {
