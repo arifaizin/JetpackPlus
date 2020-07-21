94498c4b24a5784e8643a0c6f2419badece7bdf1
Arif <faizin.arief@gmail.com>
2020-07-21 09:49:29 +0700

    add: sql cipher

--- app/src/main/java/com/arif/jetpackpro/di/DatabaseModule.kt
+++ app/src/main/java/com/arif/jetpackpro/di/DatabaseModule.kt
@@ -6,17 +6,25 @@ import com.arif.jetpackpro.room.MovieDao
 import com.arif.jetpackpro.room.MovieDatabase
 import dagger.Module
 import dagger.Provides
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
+
 
 //TODO 3 : Add @Module - Ngasih tau dagger bagaimana suatu kelas terbentuk
 @Module
 class DatabaseModule {
     @Singleton // We only want one retrofit instance at any time
     @Provides // Tells Dagger that the Room instance will come from here
-    fun provideDatabase(context: Context): MovieDatabase = Room.databaseBuilder(
-        context,
-        MovieDatabase::class.java, "Movie.db"
-    ).build()
+    fun provideDatabase(context: Context): MovieDatabase {
+        val userEnteredPassphrase = charArrayOf('a', 'r', 'i', 'f')
+        val passphrase: ByteArray = SQLiteDatabase.getBytes(userEnteredPassphrase)
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            MovieDatabase::class.java, "Movie.db"
+        ).openHelperFactory(factory).build()
+    }
 
     @Singleton
     @Provides
